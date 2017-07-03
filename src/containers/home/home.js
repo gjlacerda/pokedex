1f71b75d668e4e4094177f97392aa2351aef9a75
gjlacerda <guilherme.lcd@gmail.com>
2017-07-02 21:02:16 -0300

    Added Component styled

--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PokemonList from '~/src/components/pokemon-list/Pokemon-list';
+import {Container} from '~/src/styled/container.styled';
 
 class Home extends React.Component {
 
     render() {
         return (
-            <PokemonList/>
+            <Container>
+                <PokemonList/>
+            </Container>
         );
     }
 }
