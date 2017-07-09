6547cbe5d380a896b5954e4e11949a51e8d80faa
gjlacerda <guilherme.lcd@gmail.com>
2017-07-09 20:26:35 -0300

    Remove unused constructor

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -3,14 +3,7 @@ import ReactDOM from 'react-dom';
 import {Container, Content, Close} from 'styled/components/modal/modal.styled';
 
 export default class Modal extends React.Component {
-
-    constructor(props) {
-
-        super(props);
-
-        console.log(this.props.pokemon);
-    }
-
+    
     render() {
         return (
             <Container>
