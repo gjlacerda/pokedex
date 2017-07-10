1d42ea8cc166e470fb920b9fce1b6a1cf72cc003
gjlacerda <guilherme.lcd@gmail.com>
2017-07-09 23:52:38 -0300

    Added source sans pro font

--- src/styled/app/global.styled.js
+++ src/styled/app/global.styled.js
@@ -2,7 +2,11 @@ import {injectGlobal} from 'styled-components';
 import {bodyBg} from 'styled/app/colors.styled';
 
 injectGlobal`
+
+    @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700');
+
     body {
+        font-family: 'Source Sans Pro', sans-serif;
         background-color: ${bodyBg};
     }
     
