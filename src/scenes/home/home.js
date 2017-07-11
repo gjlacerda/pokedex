0f37270d91dc647b9d0848a8fccab8e928019805
gjlacerda <guilherme.lcd@gmail.com>
2017-07-11 18:54:39 -0300

    Refactor

--- src/scenes/home/home.js
+++ src/scenes/home/home.js
@@ -2,21 +2,16 @@ import React from 'react';
 import PokemonList from 'components/pokemon-list/Pokemon-list';
 import {Container} from 'styled/app/container.styled';
 
-class Home extends React.Component {
+export default class Home extends React.Component {
 
     render() {
         return (
             <main>
-
                 <Container>
                     <PokemonList/>
                 </Container>
-
                 <div id="modalContainer"></div>
-
             </main>
         );
     }
 }
-
-export default Home;
