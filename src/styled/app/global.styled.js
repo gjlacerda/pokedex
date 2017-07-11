ebd4abdebc6052ba699906d533fd3256b5c6b376
gjlacerda <guilherme.lcd@gmail.com>
2017-07-11 16:54:00 -0300

    Added global style to remove margins and padding of UL element

--- src/styled/app/global.styled.js
+++ src/styled/app/global.styled.js
@@ -11,6 +11,12 @@ injectGlobal`
         background-color: ${bodyBg};
     }
     
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+    
     * {
         box-sizing: border-box;
     }
