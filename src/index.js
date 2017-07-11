7e25a1944ef744b7338610265b5cce2645bcd141
gjlacerda <guilherme.lcd@gmail.com>
2017-07-11 19:36:47 -0300

    Added routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './scenes/home/Home';
+import Routes from 'config/routes';
 import 'file-loader?name=index.html!./views/index.html';
 import 'styled/app/global.styled';
 
 ReactDOM.render(
-    <Home/>,
+    <Routes/>,
     document.getElementById('app')
 );
