9357f85e81920531e3ffe766a8aba75006cedf5e
gjlacerda <guilherme.lcd@gmail.com>
2017-07-11 18:28:45 -0300

    Renamed directory from Container to Scenes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './containers/home/Home';
+import Home from './scenes/home/Home';
 import 'file-loader?name=index.html!./views/index.html';
 import 'styled/app/global.styled';
 
