f48bfbc4d3329b3aaf2431e9d5c042a57e01683a
gjlacerda <guilherme.lcd@gmail.com>
2017-07-12 22:10:04 -0300

    Created dist to production

--- src/styled/app/colors.styled.js
+++
@@ -1,5 +0,0 @@
-export const bodyBg = '#fafafa';
-
-export const grayLight = '#e6e6e6';
-export const grayDark = '#aaa';
-export const grayBody = '#555';
--- src/styled/app/container.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-    max-width: 920px;
-    margin: 0 auto;
-    padding: 0 5px;
-`;
