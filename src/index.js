0e7093620a58c82328f3932ef6bf0b46b95567f3
gjlacerda <guilherme.lcd@gmail.com>
2017-07-13 20:26:08 -0300

    Tests in production

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import Routes from 'config/routes';
+//import Header from  'scenes/header/header';
+//import 'styled/app/global.styled';
+//
+//ReactDOM.render(
+//    <div>
+//        <Header/>
+//        <Routes/>
+//    </div>
+//    ,
+//    document.getElementById('app')
+//);
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from 'config/routes';
-import Header from  'scenes/header/header';
-import 'styled/app/global.styled';
 
 ReactDOM.render(
-    <div>
-        <Header/>
-        <Routes/>
-    </div>
+    <div>Hello World</div>
     ,
     document.getElementById('app')
 );
